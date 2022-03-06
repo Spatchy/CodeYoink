cb1319f4459e222b44aca18be07f0c32edf8ba4d
Spatchy <spatchy@spatchy.net>
2022-03-06 21:41:24 +0000

    added message communication to content scripts

--- background.js
+++ background.js
@@ -8,6 +8,12 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 
   if(msg.request == "changeSettings") {
     chrome.storage.sync.set(msg.payload)
+    // propagate msg to content-scripts on all tabs
+    chrome.tabs.query({}, (tabs) => {
+      tabs.forEach( tab => {
+        chrome.tabs.sendMessage(tab.id, msg)
+      }) 
+    });
   }
 })
 
--- content-script.js
+++ content-script.js
@@ -1,27 +1,39 @@
-const modifier = "alt"
+chrome.runtime.sendMessage({request: "getSettings"}, (response) => {
+  const settings = response
 
-function isModifierHeld(evt) {
-  if(!modifier) {
+  // listen for incoming messages
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if(message.request == "changeSettings") {
+      settings = message.payload
+    }
     return true
-  } else if(modifier == "ctrl") {
-    if(evt.ctrlKey) {
-      return true
-    }
-  } else if(modifier == "shift") {
-    if(evt.shiftKey) {
-      return true
-    }
-  } else if(modifier == "alt") {
-    if(evt.altKey) {
-      return true
-    }
-  }
-  return false
-}
+  });
 
-$("code").on("click", function (evt) {
-  if(isModifierHeld(evt)) {
-    console.log("code copied!")
-    navigator.clipboard.writeText($(this).text())
+  function isModifierHeld(evt) {
+    const modifier = settings.modifier
+    if(!modifier) {
+      return true
+    } else if(modifier == "ctrl") {
+      if(evt.ctrlKey) {
+        return true
+      }
+    } else if(modifier == "shift") {
+      if(evt.shiftKey) {
+        return true
+      }
+    } else if(modifier == "alt") {
+      if(evt.altKey) {
+        return true
+      }
+    }
+    return false
   }
+
+  $("code").on("click", function (evt) {
+    if(isModifierHeld(evt)) {
+      console.log("code copied!")
+      navigator.clipboard.writeText($(this).text())
+    }
+  })
+
 })
