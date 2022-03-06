af11005cfae1b3391cfe4a0dcc1ec6b6972096cc
Spatchy <spatchy@spatchy.net>
2022-03-06 20:12:24 +0000

    added message handler for changeSettings

--- background.js
+++ background.js
@@ -1,9 +1,13 @@
 // handle incoming messages
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if(msg == "getSettings") {
+  if(msg.request == "getSettings") {
     chrome.storage.sync.get('settings', (data) => {
       sendResponse(data.settings)
     })
+  }
+
+  if(msg.request == "changeSettings") {
+    chrome.storage.sync.set(msg.payload)
   }
 })
 
