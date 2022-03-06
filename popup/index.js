3f31bbc8421f02eda4c03e66ce0734328ffd0733
Spatchy <spatchy@spatchy.net>
2022-03-06 20:12:03 +0000

    implemented basic message sending

--- popup/index.js
+++ popup/index.js
@@ -1,9 +1,9 @@
-const settings = {}
+chrome.runtime.sendMessage({request: "getSettings"}, (response) => {
+  const settings = response
+  $("#modifier_dropdown").val(settings.modifier)
 
-chrome.runtime.sendMessage("getSettings", (response) => {
-  Object.assign(settings, response)
-})
-
-$("#modifier_dropdown").change(function() {
-  settings.modifier = this.value
+  $("#modifier_dropdown").change(function() {
+    settings.modifier = this.value
+    chrome.runtime.sendMessage({request: "changeSettings", payload: settings})
+  })
 })
