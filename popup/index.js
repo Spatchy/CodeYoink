1d0a1650dafdbc87752e0b847bb5cf8d7bdf47d8
Spatchy <spatchy@spatchy.net>
2022-03-07 19:49:15 +0000

    implemented history display and actions

--- popup/index.js
+++ popup/index.js
@@ -1,3 +1,20 @@
+function generateHistoryEntry(entry) {
+  const historyTemplate = `
+  <div id="${entry.timestamp}">
+    <div class="header">
+      <span class="timeCopied"></span>
+      <a class="urlbtn headerbtn" href="${entry.url}">http</a>
+      <button class="delbtn headerbtn" id="del-${entry.timestamp}">X</button>
+    </div>
+    <div class="body">
+      <span class="preview">${entry.content.substring(0, 50)}</span>
+    </div>
+  </div>
+  `
+
+  return historyTemplate
+}
+
 chrome.runtime.sendMessage({request: "getSettings"}, (response) => {
   const settings = response
   console.log(JSON.stringify(response))
@@ -14,9 +31,26 @@ chrome.runtime.sendMessage({request: "getSettings"}, (response) => {
   chrome.runtime.sendMessage({request: "getHistory"}, (response) => {
     const history = response
     console.log("got history: " + JSON.stringify(history))
+    history.forEach(entry => {
+      const entryHtml = generateHistoryEntry(entry)
+      $("#historyFeed").prepend(entryHtml)
+    });
   })
 
   $("#clearHistoryBtn").on("click", function() {
     chrome.runtime.sendMessage({request: "clearHistory"})
+  })
+
+  $(document).on("click", ".urlbtn", function() {
+    console.log("button clicked, redirecting to: " + $(this).attr('href'))
+    chrome.tabs.create({url: $(this).attr('href')});
+    return false;
+  })
+
+  $(document).on("click", ".delbtn", function() {
+    const idToDelete = $(this).attr("id").replace("del-", "")
+    console.log("deleting: " + idToDelete)
+    $("#" + idToDelete).remove()
+    chrome.runtime.sendMessage({"deleteFromHistory": idToDelete})
   })
 })
